142f10b224a980cd3613df6cbcb27f1d870eb2bd
Brett <brettsantema@gmail.com>
2024-01-06 14:36:20 -0600

    start attacks

---
+++ Assets/Scripts/Weapons/BasicSword.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSword : MonoBehaviour
+{
+    // TODO make this a SO ?
+    public GameObject NeutralAttack;
+    public GameObject BlockAttack;
+    public GameObject SlideAttack;
+    public GameObject SprintAttack;
+    public GameObject SpecialAttack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
